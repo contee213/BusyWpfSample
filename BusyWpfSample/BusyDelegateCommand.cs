92b4f8f467faed584897a43845eae6e219443a0a
main <main@DESKTOP-3V3J6ME>
2018-03-02 01:21:00 +0900

    new

---
+++ BusyWpfSample/BusyDelegateCommand.cs
@@ -0,0 +1,70 @@
+﻿using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusyWpfSample
+{
+    public class BusyDelegateCommand : DelegateCommandBase
+    {
+        private Action _executeMethod;
+        private Func<bool> _canExecuteMethod;
+
+        volatile bool IsBusy;
+
+        MainViewModel _vm;
+
+        public BusyDelegateCommand(Action executeMethod)
+        {
+            this._executeMethod = executeMethod;
+            this._canExecuteMethod = new Func<bool>(() => true);
+        }
+
+        public BusyDelegateCommand(Action executeMethod, Func<bool> canExecuteMethod)
+        {
+            this._executeMethod = executeMethod;
+            this._canExecuteMethod = canExecuteMethod;
+        }
+
+        public BusyDelegateCommand(MainViewModel vm, Action executeMethod)
+        {
+            this._executeMethod = executeMethod;
+            this._canExecuteMethod = new Func<bool>(() => true);
+            this._vm = vm;
+        }
+
+        public BusyDelegateCommand(MainViewModel vm, Action executeMethod, Func<bool> canExecuteMethod)
+        {
+            this._executeMethod = executeMethod;
+            this._canExecuteMethod = canExecuteMethod;
+            this._vm = vm;
+        }
+
+        protected override bool CanExecute(object parameter)
+        {
+            return _canExecuteMethod.Invoke() && !IsBusy;
+        }
+
+        protected override async void Execute(object parameter)
+        {
+            IsBusy = true;
+            _vm.IsBusy = true;
+            RaiseCanExecuteChanged();
+
+            await Execute();
+
+            IsBusy = false;
+            _vm.IsBusy = false;
+            RaiseCanExecuteChanged();
+        }
+
+        public async Task Execute()
+        {
+            await Task.Run(_executeMethod);
+        }
+
+
+    }
+}
---
+++ BusyWpfSample/MainViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+using System.Windows.Input;
+using Prism.Commands;
+using System.Threading;
+
+namespace BusyWpfSample
+{
+    public class MainViewModel : BindableBase
+    {
+        private bool isBusy;
+
+        private ICommand customActionCommand;
+
+        public bool IsBusy
+        {
+            get { return isBusy; }
+            set { SetProperty(ref isBusy, value); }
+        }
+
+        public ICommand CustomActionCommand
+        {
+            get { return customActionCommand; }
+            set { SetProperty(ref customActionCommand, value); }
+        }
+
+        public MainViewModel()
+        {
+            // CustomActionCommand = new DelegateCommand(CustomAction);
+            CustomActionCommand = new BusyDelegateCommand(this, CustomAction2, CanCustomAction2);
+        }
+
+        private bool CanCustomAction2()
+        {
+            return true;
+        }
+
+        public async void CustomAction()
+        {
+            IsBusy = true;
+
+            await CustomActionImpl();
+
+            IsBusy = false;
+        }
+
+        public Task CustomActionImpl()
+        {
+            return Task.Run(() => 
+            {
+                Thread.Sleep(5000);
+            });
+        }
+
+        public void CustomAction2()
+        {
+            Console.WriteLine("Start");
+
+            Thread.Sleep(5000);
+
+            Console.WriteLine("End");
+        }
+    }
+}
